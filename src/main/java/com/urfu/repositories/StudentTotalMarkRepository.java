d557cbb9861d479cda7a0b69ede20026a2e419bd
aiperminov <a.i.perminov@urfu.ru>
2020-03-02 10:40:04 +0500

    Подготовка к тестированию. Временно убрано поле isMinor. Написан тестовый класс - пока не работает как надо

--- src/main/java/com/urfu/repositories/StudentTotalMarkRepository.java
+++ src/main/java/com/urfu/repositories/StudentTotalMarkRepository.java
@@ -7,17 +7,22 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
+/**
+ * @author aperminov
+ * 31.01.2020
+ */
 public interface StudentTotalMarkRepository extends CrudRepository<StudentTotalMark, Integer> {
 
     @Query("from StudentTotalMark stm where substring(stm.studentId, 1, 32) = :studentId")
     Iterable<StudentTotalMark> findMarksByStudentId(@Param("studentId") String studentId);
 
+    // TODO поле isMinor убрано для тестирования. Что-то надо с этим делать
     @Query("select distinct stm.disciplineLoad from StudentTotalMark stm " +
             "where substring(stm.studentId, 1, 32) = :studentId " +
             "and stm.disciplineLoad.eduYear = :eduYear " +
             "and stm.disciplineLoad.termType = :termType " +
             "and stm.disciplineLoad.hasNewRegister = false " +
-            "and stm.isMinor = false")
+            "and stm.isMinor = true")
     Iterable<DisciplineLoad> findAllDisciplineLoadByStudentYearSemesterInOldRegister(@Param("studentId") String studentId,
                                                                                      @Param("eduYear") int eduYear,
                                                                                      @Param("termType") String termType);

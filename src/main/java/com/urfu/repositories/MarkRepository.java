fee856aa247954b2002ea100e5a6ccc513a3b1b0
papajuan <a.i.perminov@urfu.ru>
2020-06-10 10:55:44 +0500

    добавлено - не брать в расчет если factor = 0

--- src/main/java/com/urfu/repositories/MarkRepository.java
+++ src/main/java/com/urfu/repositories/MarkRepository.java
@@ -2,11 +2,13 @@ package com.urfu.repositories;
 
 import com.urfu.entities.Mark;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author aperminov
  * 02.03.2020
  */
+@Repository
 public interface MarkRepository extends CrudRepository<Mark, Integer> {
 
 }
